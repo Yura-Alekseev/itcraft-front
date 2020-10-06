87ea146727c28197724c6687f8374c2b9024b404
Yura Alekseev <yuraalekseev00@gmail.com>
2020-10-06 18:59:52 +0300

    test

---
+++ src/app/account/register.component.ts
@@ -0,0 +1,63 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService, AlertService } from '@app/_services';
+
+@Component({ templateUrl: 'register.component.html' })
+export class RegisterComponent implements OnInit {
+    form: FormGroup;
+    loading = false;
+    submitted = false;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        private alertService: AlertService
+    ) { }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            name: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(255)]],
+            confirm: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(255)]]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        if (this.form.value.confirm === this.form.value.password) {
+          this.loading = true;
+          this.accountService.register(this.form.value)
+            .pipe(first())
+            .subscribe({
+              next: () => {
+                this.alertService.success('Registration successful', { keepAfterRouteChange: true });
+                this.router.navigate(['../login'], { relativeTo: this.route });
+              },
+              error: error => {
+                this.alertService.error(error);
+                this.loading = false;
+              }
+            });
+        } else {
+          this.alertService.error('Passwords do not match', { keepAfterRouteChange: true });
+        }
+    }
+}
